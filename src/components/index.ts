997b48da5ab796ecdbe47b23674b196df7f62ed1
Reupen Shah <reupen@users.noreply.github.com>
2025-02-09 00:56:15 +0000

    Add Civilization VII post

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
+import CaptionedImage from "./CaptionedImage.astro"
 import ImageGrid from "./ImageGrid.astro"
 import Pagination from "./Pagination.astro"
 import PostImage from "./PostImage.astro"
 
-export { ImageGrid, Pagination, PostImage }
+export { ImageGrid, Pagination, CaptionedImage, PostImage }
