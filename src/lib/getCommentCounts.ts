53c4cf390c3bd4f0ce0764505d5944b8ef0e0dbe
Reupen Shah <reupen@users.noreply.github.com>
2026-01-13 22:23:43 +0000

    Fix build-time comment count fetching with latest Comentario

--- src/lib/getCommentCounts.ts
+++ src/lib/getCommentCounts.ts
@@ -1,9 +1,14 @@
 import ky from "ky"
 
-export async function getCommentCounts(host: string, paths: string[]) {
+export async function getCommentCounts(
+  host: string,
+  paths: string[],
+  origin?: string,
+) {
   const response = await ky.post<{
     commentCounts?: Record<string, number>
   }>("https://comments.yuo.be/api/embed/comments/counts", {
+    headers: origin ? { origin } : undefined,
     json: {
       host,
       paths,
